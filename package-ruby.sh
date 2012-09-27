de5fce68e0b9409422686cbd8116637b266442d0
John Barton (joho) <jrbarton@gmail.com>
2012-09-27 13:44:10 +1000

    Install ruby if I aint got it.

--- package-ruby.sh
+++ package-ruby.sh
@@ -7,15 +7,20 @@ set -x
 sudo apt-get -y update
 sudo apt-get -y install libssl-dev libreadline-dev libyaml-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison libffi-dev
 
+# bootstrap off the p0 to get the gem installed.
+if [[ ! $(which ruby) ]]; then
+  sudo apt-get -y install ruby1.9.3
+fi
+
 # for some reason this installs over and over again. skip it if we can.
-if [[ ! -x /opt/ruby/bin/fpm ]]; then
+if [[ ! $(which fpm) ]]; then
   sudo gem install fpm
 fi
 
 RUBY_VERSION="1.9.3-p194"
 SOURCE_FILE_NAME=ruby-${RUBY_VERSION}
 INSTALL_DIR=/tmp/installdir
-WORKING_DIRECTORY=~/source
+WORKING_DIRECTORY=/tmp/working
 PACKAGE_NAME=goodfilms-${SOURCE_FILE_NAME}_x86_64.deb
 
 # clean any previous build artifacts
@@ -28,7 +33,7 @@ cd ${WORKING_DIRECTORY}
 
 # Download & compile ruby
 if [[ ! -f ${SOURCE_FILE_NAME}.tar.gz ]]; then
-  wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/${SOURCE_FILE_NAME}.tar.gz
+  wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/${SOURCE_FILE_NAME}.tar.gz
 fi
 tar -zxvf ${SOURCE_FILE_NAME}.tar.gz
 cd ${SOURCE_FILE_NAME}
@@ -36,7 +41,7 @@ time (./configure --prefix=/usr --with-opt-dir=/usr/local && make && make instal
 cd ..
 
 # package up the newly compiled ruby
-fpm -s dir -t deb -n ruby -v 1.9.3-p194 -C /tmp/ruby193 \
+fpm -s dir -t deb -n ruby -v 1.9.3-p194 -C ${INSTALL_DIR} \
   -p ruby-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
   -d "libc6 (>= 2.6)" -d "libffi6 (>= 3.0.10)" -d "libgdbm3 (>= 1.8.3)" \
   -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
